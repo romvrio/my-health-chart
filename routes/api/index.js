2523636d926b8bf478ef6698b7d4acd13d5cf4c6
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-08 02:30:08 -0500

    added more code in index.js in api folder & name chng to comment-routes.js (not comments)

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const testRoutes = require('./test-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
+router.use('/tests', testRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
