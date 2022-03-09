ed05f15d5fdd48210e06593dc18975dec63f6828
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-08 22:50:21 -0500

    reupdated server.js file with sessions, 2 min autoexpire & handlebars code

--- server.js
+++ server.js
@@ -1,25 +1,51 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+
+const path = require('path');
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
 const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
+    cookie: {
+        // Session will automatically expire in 2 minutes
+        expires: 2 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
 };
 
+app.use(session(sess));
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on all the routes
 app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
